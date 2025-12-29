a83c34cdf1f5788838e0c59247e5a42235b5d724
engineertareq <engineertareqbd@gmail.com>
2025-12-29 12:56:25 +0600

    laravel routing & blade templating

---
+++ app1/resources/views/about.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.default')
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <div>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic laboriosam minus, beatae et quae quo laudantium numquam nisi aliquid ipsam, vitae quasi laborum amet, odio fuga quibusdam voluptate id delectus corrupti magnam sint molestias ducimus architecto quidem. Quis exercitationem nesciunt, laboriosam perferendis rerum, fuga in reprehenderit culpa alias aliquam quae consectetur temporibus? Asperiores, odit dicta ullam explicabo delectus, excepturi, consequatur eaque possimus maiores laborum sunt eum beatae autem praesentium perspiciatis officia expedita architecto! Laborum dignissimos consequatur enim distinctio aliquid ratione porro iste non voluptates, quod maxime et incidunt modi id odio molestiae aperiam quae omnis. Facilis illo hic, totam tempora inventore molestias reprehenderit dolore quod ad, odio dolores atque ratione rem quo quam beatae, asperiores alias! Alias nostrum maiores, consequatur adipisci beatae officiis doloremque iure similique atque cupiditate quidem dignissimos et minus eaque eligendi, corporis minima fugit quibusdam sapiente possimus voluptatem, esse animi reiciendis error? Odio ducimus hic, in eligendi nobis rerum porro dignissimos illum quas molestiae asperiores dolor ipsam nam quo illo est ullam? Harum dolor non assumenda natus deleniti expedita neque qui eaque at corporis ipsum voluptatum ipsam, eius totam repudiandae corrupti odio ratione alias perspiciatis pariatur, aperiam minus soluta sint! Esse voluptas quae, voluptatum consequuntur quidem quia. Adipisci corrupti necessitatibus nemo assumenda iste optio, nostrum quod ullam consequatur distinctio repellat earum dolore provident voluptatum non velit, ratione nesciunt nihil. Perspiciatis tenetur vel hic dicta delectus error iste dignissimos, aliquid voluptate tempore illo placeat veniam consectetur, aut repellat architecto sint autem consequuntur fugiat asperiores totam at nisi enim quasi. Assumenda quia a suscipit temporibus. Consequuntur excepturi officiis necessitatibus ex incidunt debitis inventore autem placeat. Ipsa suscipit dolor accusamus, odio illum veritatis tempora, fugiat deserunt itaque possimus voluptate in vero expedita corporis ea cumque quibusdam, reprehenderit reiciendis nesciunt quasi eaque. Quo dolores, officiis ullam dignissimos quae quia est officia.
+   </div>
+</body>
+</html>
+@endsection
