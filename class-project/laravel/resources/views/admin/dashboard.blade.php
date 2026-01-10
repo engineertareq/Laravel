9cd467ae2a82bde9e3bbd80af8deb645b728588f
Tanjimul Islam Tareq <engineertareqbd@gmail.com>
2026-01-10 22:23:41 +0600

    gjjd

---
+++ class-project/laravel/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,178 @@
+@extends("backend.layouts.master")
+
+@section("head")
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicon.png') }}">
+    <title>AdminBite admin Template - The Ultimate Multipurpose admin template</title>
+    <link href="{{ asset('assets/libs/chartist/dist/chartist.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/extra-libs/c3/c3.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/libs/morris.js/morris.css') }}" rel="stylesheet">
+    <link href="{{ asset('dist/css/style.min.css') }}" rel="stylesheet">
+    </head>
+@endsection
+
+@section("content")
+ <div class="page-wrapper">
+            <div class="page-breadcrumb">
+                <div class="row">
+                    <div class="col-5 align-self-center">
+                        <h4 class="page-title">Dashboard</h4>
+                        <div class="d-flex align-items-center">
+
+                        </div>
+                    </div>
+                    <div class="col-7 align-self-center">
+                        <div class="d-flex no-block justify-content-end align-items-center">
+                            <nav aria-label="breadcrumb">
+                                <ol class="breadcrumb">
+                                    <li class="breadcrumb-item">
+                                        <a href="#">Home</a>
+                                    </li>
+                                    <li class="breadcrumb-item active" aria-current="page">Library</li>
+                                </ol>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
+            <div class="container-fluid">
+               
+                <div class="card-group">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="m-r-10">
+                                    <span class="btn btn-circle btn-lg bg-danger">
+                                        <i class="ti-clipboard text-white"></i>
+                                    </span>
+                                </div>
+                                <div>
+                                    New projects
+                                </div>
+                                <div class="ml-auto">
+                                    <h2 class="m-b-0 font-light">23</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="m-r-10">
+                                    <span class="btn btn-circle btn-lg btn-info">
+                                        <i class="ti-wallet text-white"></i>
+                                    </span>
+                                </div>
+                                <div>
+                                    Total Earnings
+
+                                </div>
+                                <div class="ml-auto">
+                                    <h2 class="m-b-0 font-light">113</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="m-r-10">
+                                    <span class="btn btn-circle btn-lg bg-success">
+                                        <i class="ti-shopping-cart text-white"></i>
+                                    </span>
+                                </div>
+                                <div>
+                                    Total Sales
+
+                                </div>
+                                <div class="ml-auto">
+                                    <h2 class="m-b-0 font-light">43</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center">
+                                <div class="m-r-10">
+                                    <span class="btn btn-circle btn-lg bg-warning">
+                                        <i class="mdi mdi-currency-usd text-white"></i>
+                                    </span>
+                                </div>
+                                <div>
+                                    Profit
+
+                                </div>
+                                <div class="ml-auto">
+                                    <h2 class="m-b-0 font-light">63</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                
+                <div class="row">
+                    <div class="col-md-6 col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex align-items-center">
+                                    <div>
+                                        <h4 class="card-title">Product Sales</h4>
+                                        <h5 class="card-subtitle">Overview of Latest Month</h5>
+                                    </div>
+                                    <div class="ml-auto">
+                                        <ul class="list-inline font-12 dl m-r-10">
+                                            <li class="list-inline-item">
+                                                <i class="fas fa-dot-circle text-info"></i> Ipad
+                                            </li>
+                                            <li class="list-inline-item">
+                                                <i class="fas fa-dot-circle text-danger"></i> Iphone</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div id="product-sales" style="height:305px"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                    
+               
+                  
+                </div>
+            
+              
+            </div>
+            
+      
+          
+          
+        </div>
+@endsection
+
+@section("script")
+    <script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('dist/js/app.min.js') }}"></script>
+    <script src="{{ asset('dist/js/app.init.js') }}"></script>
+    <script src="{{ asset('dist/js/app-style-switcher.js') }}"></script>
+    <script src="{{ asset('assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js') }}"></script>
+    <script src="{{ asset('assets/extra-libs/sparkline/sparkline.js') }}"></script>
+    <script src="{{ asset('dist/js/waves.js') }}"></script>
+    <script src="{{ asset('dist/js/sidebarmenu.js') }}"></script>
+    <script src="{{ asset('dist/js/custom.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/chartist/dist/chartist.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js') }}"></script>
+    <script src="{{ asset('assets/extra-libs/c3/d3.min.js') }}"></script>
+    <script src="{{ asset('assets/extra-libs/c3/c3.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/raphael/raphael.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/morris.js/morris.min.js') }}"></script>
+
+    <script src="{{ asset('dist/js/pages/dashboards/dashboard1.js') }}"></script>
+@endsection
